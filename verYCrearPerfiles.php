f56adc27309e9295f63eaac983582f6c6203e74f
jotarMicael <gameuxlegacy@gmail.com>
2020-05-15 19:00:53 -0300

    Update verYCrearPerfiles.php

--- verYCrearPerfiles.php
+++ verYCrearPerfiles.php
@@ -27,22 +27,25 @@
 </head>
 <body background="Imagenes/2.jpg">
 	<div>
-			<img src="Imagenes/Titulo.png" class="imagenTituloRegistro">
-			<div class="divRegistro">
-				<div class="registro">
+		<img src="Imagenes/Titulo.png" class="imagenTituloRegistro">
+		<div class="divRegistro">
+			<div class="registro">
 					<h3 class="tituloSecundarioRegistro"> Crear Perfil </h3>
 				<form action="verYCrearPerfiles.php" method="post" enctype="multipart/form-data">
-				<label class="labelWhite">Nombre: </label><br>
-				<input type="text" class="redondeado" id="nombre" name="nombre"><br>
-				<label class="labelWhite">Seleccionar imagen: </label>
-				<input type="file" class="redondeado" id="imagen" name="imagen" accept="image/png,image/jpeg"><br>
-				<button type="submit" class="boton"><strong>Aceptar</strong></button>
+					<label class="labelWhite">Nombre: </label><br>
+					<input type="text" class="redondeado" id="nombre" name="nombre"><br>
+					<label class="labelWhite">Seleccionar imagen: </label>
+					<input type="file" class="redondeado" id="imagen" name="imagen" accept="image/png,image/jpeg"><br>
+					<button type="submit" class="boton"><strong>Aceptar</strong></button>
 			  	</form>
-		    	</div>
-				<div class="registro">
+		    </div>
+		<div class="registro">
 				<h3 class="tituloSecundarioRegistro"> Perfiles creados </h3>
 				<!--En esta parte del codigo hay que consultar a la base de datos todos los perfiles que tiene cargados, y mostrarlos como un link. Ese link debe redireccionar al Home o Index.-->
 				<?php 
+					if (!empty($_FILES['imagen'])) {
+						echo "No hay perfiles creados";
+					}
 					$sql="SELECT * from perfiles";
 					$result=mysqli_query($conexion,$sql);
 
