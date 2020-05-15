b36b82def57e5a59eed9dbd2a8c4822d59657042
jotarMicael <gameuxlegacy@gmail.com>
2020-05-15 20:20:57 -0300

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -2,21 +2,25 @@
 	// Parametros a configurar para la conexion de la base de datos 
 	$host = "localhost";    // sera el valor de nuestra BD 
 	$basededatos = "bookflix";    // sera el valor de nuestra BD 
-	$usuariodb = "micael";    // sera el valor de nuestra BD 
-	$clavedb = "12345";    // sera el valor de nuestra BD 
+	$usuariodb = "root";    // sera el valor de nuestra BD 
+	$clavedb = "4509221m";    // sera el valor de nuestra BD 
 
 	//Lista de Tablas
 	$tabla_db1 = "cuenta"; 	   // tabla de usuarios
-	
+
 
 	error_reporting(0); //No me muestra errores
 	
-	$conexion = new mysqli($host,$usuariodb,$clavedb,$basededatos);
+	$conexion = mysqli_connect($host,$usuariodb,$clavedb,$basededatos);
 
-
+	
 	if ($conexion->connect_errno) {
-	    echo "Nuestro sitio experimenta fallos....";
-	    exit();
+		echo "Nuestro sitio experimenta fallos....";
+		exit();
 	}
+
+	
+
+	
 
 ?>
