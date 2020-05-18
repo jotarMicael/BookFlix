fbf9b354d800fd5c6f49979e45d3532835a40ecd
jotarMicael <gameuxlegacy@gmail.com>
2020-05-18 12:01:43 -0300

    Update Configuracion.php

--- Configuracion.php
+++ Configuracion.php
@@ -23,35 +23,22 @@
 	    <div class="divBotones">
 		<a class="botonInicio" href="Configuracion.php"> Configuracion </a>
 	    </div>
+		
 	 </div>
+	 <img class="imagenTitulo" src="Imagenes\Titulo.png">
 			<h2 class="tituloSecundarioConfiguracion" >Configuración</h2>
 			<h3 class="tituloTerciarioConfiguracion" >Modifique los datos que desee: </h3>
 			<div class="divConfiguracion">
 				<form action="configurarDatosSimples.php" method="post" enctype="multipart/form-data" onsubmit="return validar();">
 				  <div class="registroConfiguracion">
 					<label class="labelWhite">Cuenta: </label><br>
-					<input type="E-mail" class="redondeado" autocomplete="on" id="cuenta" name="cuenta" value="<?php echo $_SESSION["cuenta"]; ?>"><br>
+					<input type="E-mail" class="redondeado" autocomplete="on" id="cuenta" name="cuenta" value="<?php echo $_SESSION['usuario']['cuenta']; ?>"><br>
 					<label class="labelWhite">Nombre: </label><br>
-					<input type="text" class="redondeado" autocomplete="on" id="unNombre" name="unNombre" value="<?php echo $_SESSION['usuario']['nombre']; ?>"><br>
+					<input type="text" class="redondeado" autocomplete="on" id="nombre" name="nombre" value="<?php echo $_SESSION['usuario']['nombre']; ?>"><br>
 					<label class="labelWhite">Apellido: </label><br>
-					<input type="text" class="redondeado" autocomplete="on" id="unApellido" name="unApellido" value="<?php echo $_SESSION['usuario']['apellido']; ?>"><br>
+					<input type="text" class="redondeado" autocomplete="on" id="apellido" name="apellido" value="<?php echo $_SESSION['usuario']['apellido']; ?>"><br>
 					<label class="labelWhite">Foto: </label><br>
 					<input type="file" class= "boton" accept="jpg" id="unImagen" name="unImagen"><br>
-					<div class="registro">
-					<h1 class="tituloTerciarioConfiguracion"> DATOS DE LA TARJETA </h1>
-					<form action="verificadorNuevaCuenta.php" method="post" enctype="multipart/form-data" onsubmit="return validar();"><br>
-					<label class="labelWhite">Nombre: </label><br>
-					<input type="text" class="redondeado" id="unNombre" name="unNombre"><br>
-					<label class="labelWhite">Apellido: </label><br>
-					<input type="text" class="redondeado" id="unApellido" name="unApellido"><br>
-					<label class="labelWhite">N° de Tarjeta: </label><br>
-					<input type="text" maxlength="16" class="redondeado" id="unN°Tarjeta" name="unN°Tarjeta"><br>
-					<label class="labelWhite">Codigo de Seguridad: </label><br>
-					<input type="text" maxlength="3" class="redondeado" id="unCodigo" name="unCodigo" > <br>
-					<label class="labelWhite">Fecha de Expiracion: </label><br>
-					<input type="text" maxlength="5" class="redondeado" id="unVencimiento" name="unVencimiento"> <br>	
-				</form>
-		   		</div>
 					<input type="submit" class="boton" value="Guardar Cambios"><br>
 				  </div>
 			    </form>
@@ -64,9 +51,27 @@
 					<label class="labelWhite">Repita la nueva clave: </label><br>
 					<input type="password" class="redondeado" id="unContraseña3" name="unContraseña3"><br>
 					<br>
+					</div>
 					<input type="submit" class="boton" value="Guardar Cambios"><br>
-		   	      </div>
-		   		</form>
+				</form>
+				 <div class="registro">
+					<h3 class="tituloSecundarioRegistro"> DATOS DE LA TARJETA </h3>
+					<form action="verificadorNuevaCuenta.php" method="post" enctype="multipart/form-data" onsubmit="return validar();"><br>
+					<label class="labelWhite">Nombre: </label><br>
+					<input type="text" class="redondeado" id="unNombre" name="unNombre"><br>
+					<label class="labelWhite">Apellido: </label><br>
+					<input type="text" class="redondeado" id="unApellido" name="unApellido"><br>
+					<label class="labelWhite">N° de Tarjeta: </label><br>
+					<input type="text" maxlength="16" class="redondeado" id="unN°Tarjeta" name="unN°Tarjeta"><br>
+					<label class="labelWhite">Codigo de Seguridad: </label><br>
+					<input type="text" maxlength="3" class="redondeado" id="unCodigo" name="unCodigo" > <br>
+					<label class="labelWhite">Fecha de Expiracion: </label><br>
+					<input type="text" maxlength="5" class="redondeado" id="unVencimiento" name="unVencimiento"> <br>	
+					<input type="submit" class="boton" value="Guardar Cambios"><br>
+					</form>
+		   		</div>
+		   	      
+		   		
 		    </div>
 		</form>
 	<div class="barraFin">
