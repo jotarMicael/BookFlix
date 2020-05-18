0d5da32f703bb8678fcedb024cc769e056fcfd95
jotarMicael <gameuxlegacy@gmail.com>
2020-05-18 12:19:48 -0300

    Update configurarDatosSimples.php

--- configurarDatosSimples.php
+++ configurarDatosSimples.php
@@ -6,25 +6,25 @@
 	$apellido = $_POST['apellido'];
 	$cuenta = $_POST['cuenta'];
 
-	if( (!empty($nombre)) and (preg_match('/^[a-zA-Z]+$/',$nombre)) ){
-		if( (!empty($apellido)) and (preg_match('/^[a-zA-Z]+$/',$apellido)) ){
-				if( (!empty($cuenta)) and (preg_match('/\w+@\w+\.+[a-z]/', $cuenta)) ){
-						$verificar = mysqli_query ($conexion,"SELECT nombre_Usuario FROM cuenta WHERE (cuenta = '$cuenta') AND (id != '". $_SESSION['usuario']['id_Cuenta']."') " );
+	if( (!empty($nombre))){
+		if( (!empty($apellido))){
+				if( (!empty($cuenta))){
+						$verificar = mysqli_query ($conexion,"SELECT nombre_Usuario FROM cuenta WHERE (nombre_Usuario = '$cuenta') AND (id_Cuenta != '". $_SESSION['usuario']['id_Cuenta']."') " );
 						if(mysqli_num_rows($verificar)== 0){
 
-						$consulta = "UPDATE usuarios SET nombre = '$nombre', apellido = '$apellido', email = '$cuenta' WHERE id = '" . $_SESSION['usuario']['id'] . "' ";
+						$consulta = "UPDATE cuenta SET nombre = '$nombre', apellido = '$apellido', nombre_Usuario = '$cuenta' WHERE id = '" . $_SESSION['usuario']['id_Cuenta'] . "' ";
 
 						mysqli_query($conexion, $consulta);
 
 						if(!empty($_FILES['unImagen']['tmp_name'])){
 							$fotocontenido= addslashes(file_get_contents($_FILES['unImagen']['tmp_name']));
 							$tipofoto=explode('/', $_FILES['unImagen']['type']);
-							mysqli_query($conexion, "UPDATE usuarios SET foto_contenido = '$fotocontenido', foto_tipo = '$tipofoto[1]' WHERE id = '" . $_SESSION['usuario']['id'] . "' ");
+							mysqli_query($conexion, "UPDATE usuarios SET foto_contenido = '$fotocontenido', foto_tipo = '$tipofoto[1]' WHERE id = '" . $_SESSION['usuario']['id_Cuenta'] . "' ");
 	                    }
 
 	                    $_SESSION['usuario']['nombre'] = $nombre;
 	                    $_SESSION['usuario']['apellido'] = $apellido;
-	                    $_SESSION['usuario']['cuenta'] = $cuenta;
+	                    $_SESSION['usuario']['nombre_Usuario'] = $cuenta;
 	                    header("Location: Configuracion.php");
 	                }
 	                else{
