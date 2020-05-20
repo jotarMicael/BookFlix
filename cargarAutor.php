5901cedbf8b1836d476e81384f8f041da34bd2c2
matiasmg7199 <62666941+matiasmg7199@users.noreply.github.com>
2020-05-20 18:38:48 -0300

    Voy a ir cambiando las barras superiores

--- cargarAutor.php
+++ cargarAutor.php
@@ -7,18 +7,114 @@ include('conexion.php');
 	<link href="Estilos.css" rel="stylesheet" type="text/css">
 	<link rel="shortcut icon" href="logotipo.jpg">
 	<title>Cargar Autor</title>
+	<style>
+		body{background-color: #4642B8;padding: 15px;font-family: Arial;}
+		
+		#menu{
+			background-color: #000;
+			position: relative;
+		
+		}
+
+		#menu ul{
+			list-style: none;
+			margin: 0;
+			padding: 0;
+		}
+
+		#menu ul li{
+			display: flex;
+		}
+
+		#menu ul li a{
+			color: white;
+			display: block;
+			padding: 10px 5px;
+			text-decoration: none;
+		}
+
+		#menu ul li a:hover{
+			background-color: #42B881;
+		}
+
+		.item-r{
+			float: right;
+		}
+
+		.nav li ul {
+			display:none;
+			position:absolute;
+			min-width:140px;
+
+		}
+		.nav li:hover > ul{
+
+			display:block;
+		}
+		.nav li ul li{
+			position:absolute;
+		}
+		.nav li ul li ul{
+			position:absolute;
+			right: -140px;
+			top: 0px;
+		}
+	</style>
 </head>
 <body background= "Imagenes/2.jpg">
-	<h3 class="tituloTerciarioConfiguracion">
-		<?php if (!empty($_SESSION['error'])){
-    		echo $_SESSION['error'];
-			unset($_SESSION['error']);} ?> </h3>
-	<div class="barraInicio">	
-		<div class="divBotones"> 
-		<a class="botonInicio" href="Home.php?perfil=<?php echo $_GET['perfil'];?>&img=<?php echo $_GET['img'];?>"> Inicio </a>
-	    </div>		
-	 </div>
-	 <img class="imagenTitulo" src="Imagenes\Titulo.png">
+	<div id="menu" class="barraInicio">
+			<div class="divBotones">
+			<li><a href="Home.php" class="botonInicio">Inicio</a></li>
+			</div>
+			<div class="divBotones">
+			<?php
+				$result = mysqli_query($conexion, "SELECT nombre_Usuario FROM cuentaadministrador WHERE nombre_Usuario = '".$_SESSION['usuario']['nombre_Usuario']."' ");
+				if(mysqli_num_rows($result) <> 1){
+					?>
+			<li><a class="botonInicio" href="miPerfil.php?perfil=<?php echo $_GET['perfil'];?>&img=<?php echo $_GET['img'];?>">Mi Perfil</a></li>
+			<?php }?>
+		    </div>
+		    <div class="divBotones">
+		    	<img class="imagenBarraSuperior" src="Imagenes\TituloBarraSuperior.png">
+		    </div>
+				
+		    <div class="divBotones">
+			<label class="labelWhite">Buscar: </label>
+			<input type="text" class="redondeado" autocomplete="on" id="libro" name="libro">
+			
+		    </div>
+			<div class="divBotones">
+			<?php
+				$result = mysqli_query($conexion, "SELECT nombre_Usuario FROM cuentaadministrador WHERE nombre_Usuario = '".$_SESSION['usuario']['nombre_Usuario']."' ");
+				if(mysqli_num_rows($result) <> 1){
+					?>
+			<li><a class="botonInicio" href="Configuracion.php?perfil=<?php echo $_GET['perfil'];?>&img=<?php echo $_GET['img'];?>">Configuracion</a></li>
+			<?php }?>
+			</div>
+				
+			<?php
+				$result = mysqli_query($conexion, "SELECT nombre_Usuario FROM cuentaadministrador WHERE nombre_Usuario = '".$_SESSION['usuario']['nombre_Usuario']."' ");
+				if(mysqli_num_rows($result) == 1){
+					?>
+					<ul class="nav">
+						<li><a class="botonInicio" href="" >Administrar datos</a>
+					
+						<ul>
+							<a class="botonInicio" href="cargarLibro.php?perfil=<?php echo $_GET['perfil'];?>&img=<?php echo $_GET['img'];?>">Cargar Libro</a></li>
+							<a class="botonInicio" href="cargarAutor.php?perfil=<?php echo $_GET['perfil'];?>&img=<?php echo $_GET['img'];?>">Cargar Autor</a></li>
+							<a class="botonInicio" href="cargarGenero.php?perfil=<?php echo $_GET['perfil'];?>&img=<?php echo $_GET['img'];?>">Cargar Genero</a></li>
+							<a class="botonInicio" href="cargarNoticia.php?perfil=<?php echo $_GET['perfil'];?>&img=<?php echo $_GET['img'];?>">Cargar Noticia</a></li>
+						</ul>
+						</li>
+							
+					
+					
+					</ul>
+
+				<?php
+				}
+			?>	
+	</div>
 			<h2 class="tituloSecundarioConfiguracion" >Ingrese los datos del autor</h2>
 			<div class="divConfiguracion">
 				
