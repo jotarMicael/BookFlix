cc50cfe987e744f8a7e01ae1364e0d00735ad0c8
jotarMicael <gameuxlegacy@gmail.com>
2020-05-20 13:04:06 -0300

    Update Home.php

--- Home.php
+++ Home.php
@@ -134,7 +134,7 @@
 							<div class="divLibro">
 								<a href="vistaPrevia.php?libro=<?php $mostrar['imagenTapaLibro'];?>&perfil=<?php echo $_GET['perfil'];?>&img=<?php echo $_GET['img'];?>"><image width="80%" src="/BookFlix/Portadas/<?php echo $mostrar['imagenTapaLibro'];?>"/></a><br><br>
 								<br>
-								<td> <a href="vistaPrevia.php?libro=<?php echo $mostrar['imagenTapaLibro'];?>&perfil=<?php echo $_GET['perfil'];?>&img=<?php echo $_GET['img'];?>"> <strong><?php echo $mostrar['nombre_Libro'];?> </strong> </a></td> <br> ;&nbsp;
+								<td> <a href="vistaPrevia.php?libro=<?php echo $mostrar['imagenTapaLibro'];?>&perfil=<?php echo $_GET['perfil'];?>&img=<?php echo $_GET['img'];?>"> <strong><?php echo $mostrar['nombre_Libro'];?> </strong> </a></td> <br> &nbsp;
 								<br>
 								<?php echo $mostrar['autor'];?>
 								
