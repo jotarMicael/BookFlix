c3d61bf6f2ae71bac33fd0f1603c73b737e8009d
jotarMicael <gameuxlegacy@gmail.com>
2020-05-20 16:34:19 -0300

    Update publicar.php

--- publicar.php
+++ publicar.php
@@ -4,14 +4,15 @@
 
 	if(!($_POST['publish'])){
 		$_SESSION['error'] = "No se puede publicar un mensaje vacio";
-		header('Location: cargarNoticia.php');
+		header('Location: PagInicio.php');
 		exit;
 	}
 	if(strlen($_POST['publish']>140)){
 		$_SESSION['error'] = "No se puede publicar un mensaje con mas de 140 caracteres";
-		header('Location: cargarNoticia.php');
+		header('Location: PagInicio.php');
 		exit;
 	}
+	
 	date_default_timezone_set('America/Argentina/Buenos_Aires');
 	$fecha= date('Y-m-d H:i:s');
 
