de0da7fabb9559a8f1be4f22591d3bdc137f1598
jotarMicael <gameuxlegacy@gmail.com>
2020-05-20 16:34:15 -0300

    Update eliminarNoticia.php

--- eliminarNoticia.php
+++ eliminarNoticia.php
@@ -1,7 +1,7 @@
 <?php
 	session_start();
 	include('conexion.php');
-		$id= $_GET['idNoti'];
+	$id= $_GET['idNoti'];
 	mysqli_query($conexion, "DELETE FROM noticia WHERE $id = id_Noticia ");
 
 	header('Location: Home.php');
