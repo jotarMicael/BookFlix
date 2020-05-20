9d70002cd356e5fce4717470531cbb81e27845c9
jotarMicael <gameuxlegacy@gmail.com>
2020-05-20 14:56:09 -0300

    Delete eliminarMensajeInicio.php

--- eliminarMensajeInicio.php
+++
@@ -1,8 +0,0 @@
-<?php
-	session_start();
-	include('conexion.php');
-
-	mysqli_query($conexion, "DELETE FROM noticia WHERE '" . $_GET['id_Noticia'] . "' = id_Noticia ");
-
-	header('Location: Home.php');
-?>
