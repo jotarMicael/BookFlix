062e3880b0be3100b067020e085598411b7286c7
jotarMicael <gameuxlegacy@gmail.com>
2020-05-20 19:51:22 -0300

    Update Home.php

--- Home.php
+++ Home.php
@@ -145,7 +145,7 @@
 							while($mostrar=mysqli_fetch_array($result)){
 						?>
 							<div class="divLibro">
-								<a href="vistaPrevia.php?libro=<?php $mostrar['imagenTapaLibro'];?>&perfil=<?php echo $_GET['perfil'];?>&img=<?php echo $_GET['img'];?>"><image width="80%" src="/BookFlix/Portadas/<?php echo $mostrar['imagenTapaLibro'];?>"/></a><br><br>
+								<a href="#"><image width="80%" src="/BookFlix/Portadas/<?php echo $mostrar['imagenTapaLibro'];?>"/></a><br><br>
 								<br>
 								<td> <a href="vistaPrevia.php?libro=<?php echo $mostrar['imagenTapaLibro'];?>&perfil=<?php echo $_GET['perfil'];?>&img=<?php echo $_GET['img'];?>"> <strong><?php echo $mostrar['nombre_Libro'];?> </strong> </a></td> <br> &nbsp;
 								<br>
