13e5d257a567402fca9da1e4f101552113e9071e
jotarMicael <gameuxlegacy@gmail.com>
2020-05-20 22:21:41 -0300

    Update cargarLibro.php

--- cargarLibro.php
+++ cargarLibro.php
@@ -1,5 +1,5 @@
 <?php session_start(); 
-	include('conexion.php');
+include('conexion.php');
  ?>
 <!DOCTYPE html>
 <html>
@@ -7,22 +7,119 @@
 	<link href="Estilos.css" rel="stylesheet" type="text/css">
 	<link rel="shortcut icon" href="logotipo.jpg">
 	<title>Cargar Libro</title>
+	<style>
+		body{background-color: #4642B8;padding: 15px;font-family: Arial;}
+		
+		#menu{
+			background-color: #000;
+			position: relative;
+		
+		}
+
+		#menu ul{
+			list-style: none;
+			margin: 0;
+			padding: 0;
+		}
+
+		#menu ul li{
+			display: flex;
+		}
+
+		#menu ul li a{
+			color: white;
+			display: block;
+			padding: 10px 5px;
+			text-decoration: none;
+		}
+
+		#menu ul li a:hover{
+			background-color: #42B881;
+		}
+
+		.item-r{
+			float: right;
+		}
+
+		.nav li ul {
+			display:none;
+			position:absolute;
+			min-width:140px;
+
+		}
+		.nav li:hover > ul{
+
+			display:block;
+		}
+		.nav li ul li{
+			position:absolute;
+		}
+		.nav li ul li ul{
+			position:absolute;
+			right: -140px;
+			top: 0px;
+		}
+	</style>
 </head>
 <body background= "Imagenes/2.jpg">
-	<h3 class="tituloTerciarioConfiguracion">
-		<?php if (!empty($_SESSION['error'])){
-    		echo $_SESSION['error'];
-			unset($_SESSION['error']);} ?> </h3>
-	<div class="barraInicio">	
-		<div class="divBotones"> 
-		<a class="botonInicio" href="Home.php?perfil=<?php echo $_GET['perfil'];?>&img=<?php echo $_GET['img'];?>"> Inicio </a>
-	    </div>	
-	 </div>
-	 <img class="imagenTitulo" src="Imagenes\Titulo.png">
+	<div id="menu" class="barraInicio">
+			<div class="divBotones">
+			<li><a href="Home.php" class="botonInicio">Inicio</a></li>
+			</div>
+			<div class="divBotones">
+			<?php
+				$result = mysqli_query($conexion, "SELECT nombre_Usuario FROM cuentaadministrador WHERE nombre_Usuario = '".$_SESSION['usuario']['nombre_Usuario']."' ");
+				if(mysqli_num_rows($result) <> 1){
+					?>
+			<li><a class="botonInicio" href="miPerfil.php?perfil=<?php echo $_GET['perfil'];?>&img=<?php echo $_GET['img'];?>">Mi Perfil</a></li>
+			<?php }?>
+		    </div>
+		    <div class="divBotones">
+		    	<img class="imagenBarraSuperior" src="Imagenes\TituloBarraSuperior.png">
+		    </div>
+				
+		    <div class="divBotones">
+			<label class="labelWhite">Buscar: </label>
+			<input type="text" class="redondeado" autocomplete="on" id="" name="">
+			
+		    </div>
+			<div class="divBotones">
+			<?php
+				$result = mysqli_query($conexion, "SELECT nombre_Usuario FROM cuentaadministrador WHERE nombre_Usuario = '".$_SESSION['usuario']['nombre_Usuario']."' ");
+				if(mysqli_num_rows($result) <> 1){
+					?>
+			<li><a class="botonInicio" href="Configuracion.php?perfil=<?php echo $_GET['perfil'];?>&img=<?php echo $_GET['img'];?>">Configuracion</a></li>
+			<?php }?>
+			</div>
+				
+			<?php
+				$result = mysqli_query($conexion, "SELECT nombre_Usuario FROM cuentaadministrador WHERE nombre_Usuario = '".$_SESSION['usuario']['nombre_Usuario']."' ");
+				if(mysqli_num_rows($result) == 1){
+					?>
+					<ul class="nav">
+						<li><a class="botonInicio" href="" >Administrar datos</a>
+					
+						<ul>
+							<a class="botonInicio" href="cargarLibro.php?perfil=<?php echo $_GET['perfil'];?>&img=<?php echo $_GET['img'];?>">Cargar Libro</a></li>
+							<a class="botonInicio" href="cargarAutor.php?perfil=<?php echo $_GET['perfil'];?>&img=<?php echo $_GET['img'];?>">Cargar Autor</a></li>
+							<a class="botonInicio" href="cargarGenero.php?perfil=<?php echo $_GET['perfil'];?>&img=<?php echo $_GET['img'];?>">Cargar Genero</a></li>
+							<a class="botonInicio" href="cargarNoticia.php?perfil=<?php echo $_GET['perfil'];?>&img=<?php echo $_GET['img'];?>">Cargar Noticia</a></li>
+						</ul>
+						</li>
+							
+					
+					
+					</ul>
+
+				<?php
+				}
+			?>	
+	</div>
 			<h2 class="tituloSecundarioConfiguracion" >Ingrese los datos del libro</h2>
-			<div class="divConfiguracion">	
-				  <div class="registroConfiguracion">
-				  <form method="POST" enctype="multipart/form-data">
+			<div class="divConfiguracion">
+				
+			<div class="registroConfiguracion">
+				  <form method="POST" action="cargarLibro.php" enctype="multipart/form-data">
 					<label class="labelWhite">Nombre del Libro: </label><br>
 					<input type="text" class="redondeado" autocomplete="on" id="nombreLibro" name="nombreLibro"><br>
 					<label class="labelWhite">ISBN: </label><br>
@@ -99,10 +196,12 @@
 					<input type="submit" class="boton" value="Ingresar"><br>
 					</form>
 				  </div>
+		   	</div>	
 		   	</div>
 			<?php 
-				//if ((isset($_POST['nombreCompletoAutor']))&&isset(($_POST['nombreLibro']))&&((isset($_POST['ISBN']))&&((isset($_POST['nombreEditorial']))&&((isset($_FILES['imagen']))&&((isset($_FILES['pdf']))){
-					if (isset($_POST['nombreLibro'])&&isset($_POST['ISBN'])){
+			
+				if (isset($_POST['nombreLibro'])&&isset($_POST['ISBN'])&&isset($_POST['fecha_Lanzamiento'])&&isset($_POST['fecha_Baja'])&&isset($_FILES['imagen'])&&isset($_FILES['pdf'])){
+
 					if(isset($_FILES['imagen'])){
 						$nombre_Imagen = $_FILES ['imagen']['name'];
 						$tipo_Imagen = $_FILES ['imagen']['type'];
@@ -113,7 +212,7 @@
 								$carpeta_Destino = $_SERVER ['DOCUMENT_ROOT'] . '/BookFlix/Portadas/';
 								//Mover imagen del directorio temporal al directorio escogido
 								move_uploaded_file($_FILES['imagen']['tmp_name'], $carpeta_Destino.$nombre_Imagen);
-								//header("Location: cargarLibro.php");
+								header("Location: cargarLibro.php");
 							}
 							else {
 								echo "Solo se puede subir: png, jpg, jpeg";
@@ -123,6 +222,29 @@
 						else{
 							echo "El tamaño de la imagen es demasiado grande";
 						}
+					}	
+					
+						if(isset($_FILES['pdf'])){
+							$nombre_pdf = $_FILES ['pdf']['name'];
+							$tipo_pdf = $_FILES ['pdf']['type'];
+							$tamagno_pdf = $_FILES ['pdf']['size'];
+							if ($tamagno_pdf<=100000000){
+								if(($tipo_pdf == 'application/pdf')){
+									//Ruta de la carpeta destino
+									$carpeta_Destino = $_SERVER ['DOCUMENT_ROOT'] . '/BookFlix/pdfs/';
+									//Mover imagen del directorio temporal al directorio escogido
+									move_uploaded_file($_FILES['pdf']['tmp_name'], $carpeta_Destino.$nombre_pdf);
+									//header("Location: cargarLibro.php");
+								}
+								else {
+									echo "Solo se puede subir: pdf";
+								}
+								
+							}
+							else{
+								echo "El tamaño del pdf es demasiado grande";
+							}
+						}
 						$fecha_1 = $_POST["fecha_Lanzamiento"]; //Recibe una string en formato dd-mm-yyyy 
 						$fecha_2 = $_POST["fecha_Baja"]; //Recibe una string en formato dd-mm-yyyy 
 
@@ -131,27 +253,19 @@
 
 						$inicio = date('Y-m-d',$inicio); //Lo comvierte a formato de fecha en MySQL
 						$baja = date('Y-m-d',$fin); //Lo comvierte a formato de fecha en MySQL
+					
+					$sql2= "INSERT INTO libro(nombre_Libro, id_Editorial, fecha_Lanzamiento, fecha_DeBaja, imagenTapaLibro,pdf,autor,genero,ISBN) VALUES ('" .$_POST["nombreLibro" ]."', 1,'$inicio',  '$baja','$nombre_Imagen', '$nombre_pdf','".$_POST["nombreCompletoAutor"]."','" .$_POST["genero" ]."','".$_POST["ISBN"]."')";
+					$result=mysqli_query($conexion,$sql2);
+					ob_start();
+ 					 echo "El Libro se ha cargado correctamente";
+					header("Location: Home.php");
 
-						echo $nombre_Imagen;
-						echo $_POST["nombreCompletoAutor"];
-						/*$sql1="SELECT id_Editorial from editorial WHERE nombre_Editorial = '" .$_POST["nombreEditorial"] ."' ";
-						$idEditorial=mysqli_query($conexion,$sql1);
-						echo $idEditorial;*/
-						$sql2= "INSERT INTO libro(nombre_Libro, id_Editorial, fecha_Lanzamiento, fecha_DeBaja, imagenTapaLibro,pdf,autor) VALUES ('" .$_POST["nombreLibro" ]."', '$idEditorial', '$inicio',  '$baja','$nombre_Imagen', '$nombre_pdf','".$_POST["nombreCompletoAutor"]."')";
-						$result=mysqli_query($conexion,$sql2);
-						echo "el libro se ha cargado correctamente";
-
-					}
-
+					
 				}
 
-
-
-					
-					
-				//} 
-					
-			
+				else{
+					echo "Todos los campos deben estar completos";
+				}
 			?>
 	 </div>
 		
