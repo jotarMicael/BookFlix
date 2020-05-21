a01bba809a9269560b5e9b8f1e2fe55b1aff5c7b
jotarMicael <gameuxlegacy@gmail.com>
2020-05-21 16:11:40 -0300

    Update Configuracion.php

--- Configuracion.php
+++ Configuracion.php
@@ -1,4 +1,7 @@
 <?php session_start(); 
+
+include('conexion.php');
+
  ?>
 <!DOCTYPE html>
 <html>
@@ -53,15 +56,15 @@
 					<label class="labelWhite">Repita la nueva clave: </label><br>
 					<input type="password" class="redondeado" id="unContraseña3" name="unContraseña3"><br>
 					<input type="submit" class="boton" value="Guardar Cambios"><br>
-				</form>
 					</div>
+				</form>
 				 <div class="registro">
 					<h3 class="tituloSecundarioRegistro"> DATOS DE LA TARJETA </h3>
-					<form action="verificadorNuevaCuenta.php" method="post" enctype="multipart/form-data" onsubmit="return validar();"><br>
+					<form action="Configuracion.php" method="post" enctype="multipart/form-data"><br>
 					<label class="labelWhite">Nombre: </label><br>
-					<input type="text" class="redondeado" id="unNombre" name="unNombre"><br>
+					<input type="text" class="redondeado" id="unNombreTar" name="unNombreTar"><br>
 					<label class="labelWhite">Apellido: </label><br>
-					<input type="text" class="redondeado" id="unApellido" name="unApellido"><br>
+					<input type="text" class="redondeado" id="unApellidoTar" name="unApellidoTar"><br>
 					<label class="labelWhite">N° de Tarjeta: </label><br>
 					<input type="text" maxlength="16" class="redondeado" id="unN°Tarjeta" name="unN°Tarjeta"><br>
 					<label class="labelWhite">Codigo de Seguridad: </label><br>
@@ -71,10 +74,25 @@
 					<input type="submit" class="boton" value="Guardar Cambios"><br>
 					</form>
 		   		</div>
+				   <?php
+				   				   		if (isset($_POST['unN°Tarjeta'])&&isset($_POST['unNombreTar'])&&isset($_POST['unApellidoTar'])&&isset($_POST['unCodigo'])&&isset($_POST['unVencimiento'])){
+											$consulta = "UPDATE cuentausuario SET numero_Tarjeta = '" . $_POST['unN°Tarjeta']. "', codigo_Seguridad = '" . $_POST['unCodigo']. "', nombre_Tarjeta='" . $_POST['unNombreTar']. "' , apellido_Tarjeta= '" . $_POST['unApellidoTar']. "' WHERE nombre_Usuario = '" . $_SESSION['usuario']['nombre_Usuario'] . "' ";
+				   
+											mysqli_query($conexion, $consulta);
+				   
+											echo"Datos de la tarjeta modificados correctamente";
+											
+											  }
+										   else{
+											   echo "Todos los campos de la tarjeta deben estar completos";
+										   }
+										  
+									
+				 ?>
+				 
 		   	      
 		   		
 		    </div>
-		</form>
 	<div class="barraFin">
 		<p class="textoBarra">Gutierrez Matias 15257/5 - Jotar Micael 15388/6 - Valentin Gallardo 15292/9</p>
     </div>
