e6cdc6dbda41e2159499b40c7995a6ce038ac0f5
jotarMicael <gameuxlegacy@gmail.com>
2020-06-01 13:52:52 -0300

    Update miPerfil.php

--- miPerfil.php
+++ miPerfil.php
@@ -14,10 +14,10 @@
 <body background="Imagenes/2.jpg">
 	<div class="barraInicio" >
 		<div class="divBotones">
-          <a class="botonInicio" href="Home.php?perfil=<?php echo $_GET['perfil'];?>&img=<?php echo $_GET['img'];?>"> Inicio </a>
+          <a class="botonInicio" href="Home.php?&perfil=<?php echo $_GET['perfil'];?>&img=<?php echo $_GET['img'];?>"> Inicio </a>
 	    </div>
 	    <div class="divBotones">
-          <a class="botonInicio" href="miPerfil.php?perfil=<?php echo $_GET['perfil'];?>&img=<?php echo $_GET['img'];?>">Mi Perfil</a>
+          <a class="botonInicio" href="miPerfil.php?&perfil=<?php echo $_GET['perfil'];?>&img=<?php echo $_GET['img'];?>">Mi Perfil</a>
 	    </div>
 	    <div class="divBotones">
         <form action="Busqueda.php" method="post">
@@ -26,10 +26,34 @@
         </form>
         </div>
 	    <div class="divBotones">
-            <a class="botonInicio" href="Configuracion.php?perfil=<?php echo $_GET['perfil'];?>&img=<?php echo $_GET['img'];?>"> Configuracion </a>
+            <a class="botonInicio" href="Configuracion.php?&perfil=<?php echo $_GET['perfil'];?>&img=<?php echo $_GET['img'];?>"> Configuracion </a>
 	    </div>  
 	</div>
 	<img src="Imagenes/Titulo.png" class="imagenTituloRegistro">
+	<h2 class="tituloSecundarioConfiguracion" >Lecturas Recientes...</h2>
+				
+						<?php 
+							$sql2="SELECT id_Libro from perfilleelibro where nombre_Perfil = '" .$_GET['perfil']."'";
+							$result2=mysqli_query($conexion,$sql2);
+							while($mostrar2=mysqli_fetch_array($result2)){;
+							
+
+							$sql="SELECT imagenTapaLibro,nombre_Libro,autor,id_Editorial,genero,id_Libro from libro where id_Libro = '" .$mostrar2['id_Libro']."' ";
+							$result=mysqli_query($conexion,$sql);
+							while($mostrar=mysqli_fetch_array($result)){
+						?>
+							<div class="divLibro">
+								<a href="#"><image width="80%" src="/BookFlix/Portadas/<?php echo $mostrar['imagenTapaLibro'];?>"/></a><br><br>
+								<br>
+								<td> <a href="vistaPrevia.php?&libro=<?php echo $mostrar['imagenTapaLibro'];?>&titulo=<?php echo $mostrar['nombre_Libro'];?>&autor=<?php echo $mostrar['autor'];?>&idEdi=<?php echo $mostrar['id_Editorial'];?>&genero=<?php echo $mostrar['genero'];?>&idLibro=<?php echo $mostrar['id_Libro'];?>&perfil=<?php echo $_GET['perfil'];?>"> <strong><?php echo $mostrar['nombre_Libro'];?> </strong> </a></td> <br> &nbsp;
+								<br>
+								
+						<br>
+							</div>
+						<?php 
+							 } }
+						 ?>
+				
 	<div class="fondoPerfil">
 	<div class="panelPerfil">
 		<div class="imagenPanelPerfil">
