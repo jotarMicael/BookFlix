5ea46622d54aacdc2af52b8587d13176dd70ee02
jotarMicael <gameuxlegacy@gmail.com>
2020-06-02 15:00:54 -0300

    Update verificadorBusqueda.php

--- verificadorBusqueda.php
+++ verificadorBusqueda.php
@@ -2,7 +2,7 @@
 function resultadoBusqueda(){
 	include('conexion.php');
 	$busca= $_POST['busca'];
-	$result = mysqli_query($conexion, "SELECT * FROM libro WHERE (nombre_Libro LIKE '%$busca%' or autor LIKE '%$busca%' or ISBN LIKE '%$busca%')");
+	$result=mysqli_query($conexion, "SELECT * FROM libro WHERE (nombre_Libro LIKE '$busca%' or ISBN LIKE '$busca%')");
 	return $result;
 	exit;
 }
