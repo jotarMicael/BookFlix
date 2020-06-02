1236bcd8fc858bcf50166c0b9b5701a196e88887
jotarMicael <gameuxlegacy@gmail.com>
2020-06-01 22:32:07 -0300

    Update verificadorLogin.php

--- verificadorLogin.php
+++ verificadorLogin.php
@@ -14,9 +14,10 @@
 		exit;
 	}
 	
-	 $result = mysqli_query($conexion, "SELECT * FROM cuentausuario WHERE nombre_Usuario = '" . $_POST['nickname'] . "' AND contraseña = '" . $_POST['contraseña'] . "' ");
+	 $result = mysqli_query($conexion, "SELECT * FROM cuentausuario WHERE nombre_Usuario = '" . $_POST['nickname'] . "'");
+	 $result2 = mysqli_query($conexion, "SELECT * FROM cuenta WHERE nombre_Usuario = '" . $_POST['nickname'] . "' AND contraseña = '" . $_POST['contraseña'] . "' ");
 
-	if( mysqli_num_rows($result) == 0 ){
+	if(( mysqli_num_rows($result) == 0 ) || ( mysqli_num_rows($result2) == 0 )){
 		$_SESSION['error'] = 'Usuario y/o Contraseña incorrectos';
 		header('Location: index.php');
 		exit;
