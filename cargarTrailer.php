faacf024eacb3c24c1298dd1de016f3daef1abec
jotarMicael <gameuxlegacy@gmail.com>
2020-06-04 14:57:26 -0300

    Update cargarTrailer.php

--- cargarTrailer.php
+++ cargarTrailer.php
@@ -6,58 +6,148 @@ include('conexion.php');
 <head>
 	<link href="Estilos.css" rel="stylesheet" type="text/css">
 	<link rel="shortcut icon" href="logotipo.jpg">
-	<title>Cargar Noticia</title>
+	<title>Cargar Trailer</title>
+	<style>
+		body{background-color: #4642B8;padding: 15px;font-family: Arial;}
+		
+		#menu{
+			background-color: #000;
+			position: relative;
+		
+		}
+
+		#menu ul{
+			list-style: none;
+			margin: 0;
+			padding: 0;
+		}
+
+		#menu ul li{
+			display: flex;
+		}
+
+		#menu ul li a{
+			color: white;
+			display: block;
+			padding: 10px 5px;
+			text-decoration: none;
+		}
+
+		#menu ul li a:hover{
+			background-color: #42B881;
+		}
+
+		.item-r{
+			float: right;
+		}
+
+		.nav li ul {
+			display:none;
+			position:absolute;
+			min-width:140px;
+
+		}
+		.nav li:hover > ul{
+
+			display:block;
+		}
+		.nav li ul li{
+			position:absolute;
+		}
+		.nav li ul li ul{
+			position:absolute;
+			right: -140px;
+			top: 0px;
+		}
+	</style>
 </head>
 <body background= "Imagenes/2.jpg">
-	<h3 class="tituloTerciarioConfiguracion">
-		<?php if (!empty($_SESSION['error'])){
-    		echo $_SESSION['error'];
-			unset($_SESSION['error']);} ?> </h3>
-	<div class="barraInicio">	
-		<div class="divBotones"> 
-		<a class="botonInicio" href="Home.php?perfil=<?php echo $_GET['perfil'];?>&img=<?php echo $_GET['img'];?>"> Inicio </a>
-	    </div>		
-	 </div>
-	 <img class="imagenTitulo" src="Imagenes\Titulo.png">
-			<h2 class="tituloSecundarioConfiguracion" >Ingrese el Trailer</h2>
-			
+	<div id="menu" class="barraInicio">
+			<div class="divBotones">
+			<li><a href="Home.php" class="botonInicio">Inicio</a></li>
+			</div>
+			<div class="divBotones">
+			<?php
+				$result = mysqli_query($conexion, "SELECT nombre_Usuario FROM cuentaadministrador WHERE nombre_Usuario = '".$_SESSION['usuario']['nombre_Usuario']."' ");
+				if(mysqli_num_rows($result) <> 1){
+					?>
+			<li><a class="botonInicio" href="miPerfil.php?perfil=<?php echo $_GET['perfil'];?>&img=<?php echo $_GET['img'];?>">Mi Perfil</a></li>
+			<?php }?>
+		    </div>
+		    <div class="divBotones">
+		    	<img class="imagenBarraSuperior" src="Imagenes\TituloBarraSuperior.png">
+		    </div>
 				
-			<div class="fondoComentarios">
-				<form action="publicar.php" method="POST" onsubmit="return validar();" enctype="multipart/form-data">
-				<div class="comentario">
-	    			<div class="cuerpoComentario">
-	    		<div class="barraTop-publicacion">	
-	    		<h5 class="textoBarraTop"><?php echo $_SESSION['nombrePerfil']; ?></h5>		
-	    	</div>
-	    	<div>
-	    		<textarea cols="3" rows="5" class="contenido-publicacion" name = "publish" id="publish"></textarea>
-	    	</div>
-	    	<div class="barraBot">
-	    		<input type="submit" class="botonInicio" name="Publicar" value="Publicar">
-	    	</div>
-	    
+		    <div class="divBotones">
+			<label class="labelWhite">Buscar: </label>
+			<input type="text" class="redondeado" autocomplete="on" id="libro" name="libro">
+			
+		    </div>
+			<div class="divBotones">
+			<?php
+				$result = mysqli_query($conexion, "SELECT nombre_Usuario FROM cuentaadministrador WHERE nombre_Usuario = '".$_SESSION['usuario']['nombre_Usuario']."' ");
+				if(mysqli_num_rows($result) <> 1){
+					?>
+			<li><a class="botonInicio" href="Configuracion.php?perfil=<?php echo $_GET['perfil'];?>&img=<?php echo $_GET['img'];?>">Configuracion</a></li>
+			<?php }?>
+			</div>
 	</div>
-	</form>	
+			<h2 class="tituloSecundarioConfiguracion" >Ingrese los datos del trailer</h2>
+			<div class="divConfiguracion">
+				  <div class="registroConfiguracion">
+				  <form action="cargarCapituloLibro.php" method="post" enctype="multipart/form-data">
+					<label class="labelWhite">ISBN del libro: </label><br>
+					<input type="text" class="redondeado" autocomplete="on" id="ISBN" name="ISBN"><br>
+					<label class="labelWhite">Seleccionar archivo: </label><br>
+					<input type="file" class="redondeado" id="archivo" name="archivo"><br>
+					<input type="submit" class="boton" value="Ingresar"><br>
+					</form>
+				  </div>
 				
 		   	</div>
 			<?php 
-				if (isset($_POST['publish'])){
+			
+				if (isset($_POST['ISBN'])&&isset($_FILES['archivo'])){
 
-					//Consulto en la bbdd si ya existe la noticia que quiero ingresar
-					$sql= "SELECT texto FROM noticia WHERE texto = '".$_POST['publish']."'";
-					$result=mysqli_query($conexion,$sql);
+						if(isset($_FILES['archivo'])){
+
+							$nombre_archivo=$_FILES['archivo']['name'];
+
+							$sql4="SELECT * from trailer WHERE archivo_Trailer='$nombre_archivo'";
+							$result4=mysqli_query($conexion,$sql4);
+							
+							$tipo_archivo=$_FILES['archivo']['type'];
+							$tamagno_archivo=$_FILES['archivo']['size'];
+	
+							if ($tamagno_archivo<=1000000000000){
+								
+	
+									//Ruta de la carpeta destino
+									$carpeta_Destino=$_SERVER ['DOCUMENT_ROOT'].'/BookFlix/archivos/';
+									//Mover imagen del directorio temporal al directorio escogido
+									move_uploaded_file($_FILES['archivo']['tmp_name'],$carpeta_Destino.$nombre_archivo);
+									//header("Location: cargarLibro.php");
+							}
+								
+								
+							else{
+	
+								echo "<font color=white  size='5pt'> El tamaño del archivo es demasiado grande </font>";
+							}
+						}
+
+						$sql3="SELECT id_Libro from libro WHERE ISBN ='" .$_POST['ISBN']."' ";
+						$result3=mysqli_query($conexion,$sql3);
+						$mostrar=mysqli_fetch_array($result3, MYSQLI_ASSOC);
+						
+						$sql2="INSERT INTO capitulo(nombre_Capitulo,id_Libro,archivo) VALUES ('".$_POST["nCap"]."','".$mostrar["id_Libro"]."','$nombre_archivo')";
+						$result2=mysqli_query($conexion,$sql2);
+						echo "<font color=white  size='5pt'> El capitulo se ha cargado correctamente </font>";
+
+					}
 					
-					if( mysqli_num_rows($result) == 1 ){
-						echo "<font color=white  size='5pt'>Esta noticia ya se encuentra cargada en el sistema</font>";
-						//ingreso la noticia
-					}
-					else {
-						$sql= "INSERT INTO noticia(texto) VALUES ('" .$_POST["publish"]."')";
-						$result=mysqli_query($conexion,$sql);
-						echo "<font color=white  size='5pt'> La noticia se ha cargado correctamente</font>";
-
-					}
-				}
+				
+				else echo "<font color=white  size='5pt'> Deben ingresarse todos los datos, de lo contrario, seguira mostrandose este mensaje </font>";
 			?>
 	 </div>
 		
