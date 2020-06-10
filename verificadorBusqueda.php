9ab635c2c015f772039705c54fa8bf52f4d5e7d2
jotarMicael <gameuxlegacy@gmail.com>
2020-06-10 01:36:50 -0300

    Update verificadorBusqueda.php

--- verificadorBusqueda.php
+++ verificadorBusqueda.php
@@ -2,8 +2,18 @@
 function resultadoBusqueda(){
 	include('conexion.php');
 	$busca= $_POST['busca'];
-	$result=mysqli_query($conexion, "SELECT * FROM libro WHERE (nombre_Libro = '$busca' or ISBN = '$busca')");
+	$result=mysqli_query($conexion, "SELECT *
+   FROM libro l 
+   INNER JOIN generopertenecelibro gp ON (l.id_Libro= gp.id_Libro) 
+   INNER JOIN genero g ON (g.id_Genero = gp.id_Genero) 
+   INNER JOIN editorial e ON (e.id_Editorial = l.id_Editorial) 
+   INNER JOIN autoreslibro al ON (al.id_Libro = l.id_Libro)
+   INNER JOIN autor a ON (a.id_Autor = al.id_Autor)
+   WHERE l.nombre_Libro = '".$_POST['busca']."' OR g.nombre_Genero = '".$_POST['busca']."' OR a.nombre = '".$_POST['busca']."' OR e.nombre_Editorial = '".$_POST['busca']."' ");
+	
 	return $result;
 	exit;
+
 }
 ?>
+
