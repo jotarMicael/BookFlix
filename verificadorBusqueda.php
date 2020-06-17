3777faca26bde6b4db275f5f29a5fc21485c231d
jotarMicael <gameuxlegacy@gmail.com>
2020-06-17 16:14:26 -0300

    .

--- verificadorBusqueda.php
+++ verificadorBusqueda.php
@@ -8,17 +8,19 @@ function resultadoBusqueda(){
    echo $nm[0];
    $nom=$nm[0];
    echo $busca;
-   if(isset($busca)){
-	$result=mysqli_query($conexion, "SELECT l.id_Libro,l.id_Editorial,l.fecha_Lanzamiento,l.imagenTapaLibro,l.ISBN,l.capitulos,l.nombre_Libro
-     FROM libro l 
-     INNER JOIN generopertenecelibro gp ON (l.id_Libro= gp.id_Libro) 
-     INNER JOIN genero g ON (g.id_Genero = gp.id_Genero) 
-     INNER JOIN editorial e ON (e.id_Editorial = l.id_Editorial) 
-     INNER JOIN autoreslibro al ON (al.id_Libro = l.id_Libro)
-     INNER JOIN autor a ON (a.id_Autor = al.id_Autor)
-     WHERE l.nombre_Libro LIKE '%".$_POST['busca']."%' AND g.nombre_Genero ='".$_POST['genero']."' AND a.nombre LIKE 'Mary' AND e.nombre_Editorial ='".$_POST['nombreEditorial']."' ");
+   if(empty($busca)){
+    $busca='';
    }
-   else{
+   if(($_POST['genero'])=='Todos'){
+      $_POST['genero']='';
+   }
+   if(($_POST['nombreEditorial'])=='Todos'){
+      $_POST['nombreEditorial']='';
+   }
+   if(($nm[0])=='Todos'){
+      $nm[0]='';
+ }
+
    $result=mysqli_query($conexion, "SELECT l.id_Libro,l.id_Editorial,l.fecha_Lanzamiento,l.imagenTapaLibro,l.ISBN,l.capitulos,l.nombre_Libro
      FROM libro l 
      INNER JOIN generopertenecelibro gp ON (l.id_Libro= gp.id_Libro) 
@@ -26,8 +28,7 @@ function resultadoBusqueda(){
      INNER JOIN editorial e ON (e.id_Editorial = l.id_Editorial) 
      INNER JOIN autoreslibro al ON (al.id_Libro = l.id_Libro)
      INNER JOIN autor a ON (a.id_Autor = al.id_Autor)
-     WHERE l.nombre_Libro LIKE '%%' AND g.nombre_Genero = '".$_POST['genero']."' AND a.nombre LIKE 'Mary' AND e.nombre_Editorial = '".$_POST['nombreEditorial']."' ");
-   }
+     WHERE l.nombre_Libro LIKE '%".$busca."%' AND g.nombre_Genero LIKE '%".$_POST['genero']."%' AND a.nombre LIKE '$nm[0]' AND e.nombre_Editorial LIKE '%".$_POST['nombreEditorial']."%' ");
 	return $result;
 	exit;
 
