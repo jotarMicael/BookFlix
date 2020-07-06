a7f50d63e8e65952762dab5769f939c64e6a4d4f
jotarMicael <gameuxlegacy@gmail.com>
2020-07-06 17:55:51 -0300

    .

--- detalleCapitulo.php
+++ detalleCapitulo.php
@@ -28,11 +28,11 @@ include('conexion.php');
 					<?php     $sql2="SELECT sum(valor) FROM calificacion WHERE id_Libro=74 AND nombre_Capitulo='Libro Completo' ";
 							  $result2=mysqli_query($conexion,$sql2);
 							  foreach($result2 as $row)
-							  while($mostrar2=mysqli_fetch_array($result2))	
-							  echo $row['sum(valor)'];
+							  $pr=($row['sum(valor)'])/(mysqli_num_rows($result2))
+							 
 							  
 	  				?>
-					<label class="labelWhite"> Este capitulo tiene una recomendacion de <?php echo $row['sum(valor)'];?> estrellas por los usuarios que lo han leido. </label><br><br>
+					<label class="labelWhite"> Este capitulo tiene una recomendacion de <?php echo $pr;?> estrellas por los usuarios que lo han leido. </label><br><br>
 					<a href="leerLibro.php?&idLibro=<?php echo $_GET['idLibro'];?>&perfil=<?php echo $_GET['perfil'];?>&nCap=<?php echo $_GET['nCap'];?>" class="labelWhite"><strong> Leer: <?php
 				 echo $_GET['nCap']."<br /> <br />"?>  </strong>
 					
