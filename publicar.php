c5667ed5cde7f7fa1ceb75da2ddaec07754b29e3
jotarMicael <gameuxlegacy@gmail.com>
2020-07-08 12:23:47 -0300

    .

--- publicar.php
+++ publicar.php
@@ -18,19 +18,20 @@
 		exit;
 	}
 	else{
-	if(strlen($_POST['publish']>140)){
-		$_SESSION['error'] = "No se puede publicar un mensaje con mas de 140 caracteres";
+	date_default_timezone_set('America/Argentina/Buenos_Aires');
+	$fecha= date('Y-m-d H:i:s');
+
+	if(strlen($_POST['publish'])>140){
+		$_SESSION['error']="No se puede publicar un mensaje con mas de 140 caracteres";
 		header("Location: cargarNoticia.php");
 		exit;
 	}
 	else{
-	date_default_timezone_set('America/Argentina/Buenos_Aires');
-	$fecha= date('Y-m-d H:i:s');
-
 	$result2 = mysqli_query($conexion, "INSERT INTO noticia(texto,fecha) VALUES ('" . $_POST['publish'] . "', '$fecha')");
 	header("Location: Home.php");
 	exit;
 	}
+
 }
 }
 ?>
