d5cb52afa35629001b0e9705644d63420c88d363
jotarMicael <gameuxlegacy@gmail.com>
2020-07-11 18:23:42 -0300

    .

--- eliminarReseñaAdmin.php
+++ eliminarReseñaAdmin.php
@@ -1,7 +1,7 @@
 <?php
   include('conexion.php');
   session_start();
-  $sql= "UPDATE reseña SET spoiler='Si' WHERE texto='".$_POST['texto']."'";
+  $sql= "UPDATE reseña SET reportada = 'No', spoiler = 'Si' WHERE texto = '".$_POST['texto']."' ";
   //$sql= "DELETE FROM reseña WHERE texto = '".$_POST['texto']."'  ";
   $result=mysqli_query($conexion,$sql);
   $_SESSION['error']='Comentario marcado como spoiler de forma exitosa';
